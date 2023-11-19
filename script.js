00adbbb7c29163b356338bc51850b9c13805fa47
Tomas Miguez <tomasmiguez99@gmail.com>
2023-11-19 18:14:12 -0300

    feat: add websocket syncronicity

--- script.js
+++ script.js
@@ -24,9 +24,29 @@ async function getResourceCount(resName) {
   try {
     const response = await fetch('http://localhost:8080/' + resName);
     const data = await response.json();
-    const resCountElement = document.getElementById(resName + 'Count');
-    resCountElement.innerHTML = resName.toUpperCase() + 's: ' + data.count;
+    setResourceCount(resName, data.count);
   } catch (error) {
     console.error('Error fetching ' + resName + ' count:', error);
   }
+}
+
+function setResourceCount(resName, count) {
+  const resCountElement = document.getElementById(resName + 'Count');
+
+  if (resCountElement) {
+    resCountElement.innerHTML = resName.toUpperCase() + 's: ' + count;
+  }
+}
+
+function setupWebsocket(resName) {
+      var mySocket = new WebSocket("ws://localhost:8080/ws");
+
+      mySocket.onmessage = function (event) {
+        var output = document.getElementById("output");
+        dataJson = JSON.parse(event.data);
+
+        if (dataJson.type == resName) {
+          setResourceCount(resName, dataJson.newCount);
+        }
+      };
 }
