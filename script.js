b1fa50d1736489e4bd5ef236a97dd31dc6372aac
Tomas Miguez <tomasmiguez99@gmail.com>
2023-11-19 15:06:30 -0300

    feat: add backend

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function ping() {
-  console.log('pong');
+async function ping() {
+  postResource('ping');
 }
 
 function pong() {
-  console.log('ping');
+  postResource('pong');
+}
+
+async function postResource(resName) {
+  try {
+    const response = await fetch('http://localhost:8080/' + resName, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getResourceCount(resName) {
+  try {
+    const response = await fetch('http://localhost:8080/' + resName);
+    const data = await response.json();
+    const resCountElement = document.getElementById(resName + 'Count');
+    resCountElement.innerHTML = resName.toUpperCase() + 's: ' + data.count;
+  } catch (error) {
+    console.error('Error fetching ' + resName + ' count:', error);
+  }
 }
